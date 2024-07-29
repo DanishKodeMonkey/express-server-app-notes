aca4e75b0db8ad22bc6f0e7538b4799d779a32bd
danishkodemonkey <humberjini@hotmail.com>
2024-07-29 14:51:29 +0200

    start app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+/* The app.js file
+
+App.js will server as the starting point for the Express server. 
+The file can be named anyhting we want, such as index.js, or main.js.
+For the purposes of this project, we will simply go with app.js, 
+as stated in the Express docs
+
+*/
